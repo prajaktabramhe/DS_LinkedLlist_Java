75113a4b46fa6d4163553f2f667b8ad29bfb6a47
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-26 16:17:59 +0530

    [Prajakta]: Created Linked List by adding 30,56,70

--- src/test/java/com/bridgelabz/MyNodeTest.java
+++ src/test/java/com/bridgelabz/MyNodeTest.java
@@ -8,6 +8,7 @@ public class MyNodeTest
     @Test
     public void given3NumberWhenLinkedShouldPassedLinkedListTest()
     {
+
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(70);
